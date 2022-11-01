f45e341011965146dbe344cdb700e998da687d40
hiuwehf <matheus.thomassen@outlook.com>
2022-11-01 02:21:35 +0100

    Added a welcoming div with instruction

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const chatName = document.getElementById("chatNameBar");
+const chatBox0 = document.getElementById("chatBox0");
 const chatBox1 = document.getElementById("chatBox1");
 const chatBox2 = document.getElementById("chatBox2");
 const chatBox3 = document.getElementById("chatBox3");
@@ -6,6 +7,7 @@ const chatBox3 = document.getElementById("chatBox3");
 
 function deezToggle(){
     chatName.innerHTML = "Deez";
+    chatBox0.style.display="none";
     chatBox1.style.display="block";
     chatBox2.style.display="none";
     chatBox3.style.display="none";
@@ -13,6 +15,7 @@ function deezToggle(){
 
 function ligmaToggle(){
     chatName.innerHTML = "Ligma";
+    chatBox0.style.display="none";
     chatBox1.style.display="none";
     chatBox2.style.display="block";
     chatBox3.style.display="none";
@@ -21,6 +24,7 @@ function ligmaToggle(){
 
 function sugondeseToggle(){
     chatName.innerHTML = "Sugondese";
+    chatBox0.style.display="none";
     chatBox1.style.display="none";
     chatBox2.style.display="none";
     chatBox3.style.display="block";
