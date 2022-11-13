e1bcf3ad77239a71fe0a190ef964aaf54c7d9e03
hiuwehf <matheus.thomassen@outlook.com>
2022-11-13 14:19:23 +0100

    Added text wrapping, autoscroll div

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const chatBox3 = document.getElementById("chatBox3");
 const chatContent1 = document.getElementById("chatContent1");
 const chatContent2 = document.getElementById("chatContent2");
 const chatContent3 = document.getElementById("chatContent3");
+const MessageContainer = document.getElementsByClassName("MessageContainer");
 
 
 
@@ -32,27 +33,50 @@ function sugondeseToggle(){
     chatBox2.style.display="none";
     chatBox3.style.display="block";
 }
+function scroll(){
+    MessageContainer.scrollTop += 10;
+  }
 
-function sendMessage(){
-    if(chatBox1.style.display == 'block'){
+function sendMessage1(){
+    if(chatContent1.value == " "){
+        alert("deez");
+    }else{
+        (chatBox1.style.display == 'block')
         const para = document.createElement("p");
         const node = document.createTextNode(chatContent1.value);
         para.appendChild(node);
         const element = document.getElementById("messageContainer1");
         element.appendChild(para);
-        
-    }else if(chatBox2.style.display == 'block'){
+        chatContent1.value = ' ';
+        para.scrollIntoView();
+    }
+}
+
+function sendMessage2(){
+    if(chatContent2.value == " "){
+        alert("Input text");
+    }else{
+        (chatBox2.style.display == 'block')
         const para = document.createElement("p");
         const node = document.createTextNode(chatContent2.value);
         para.appendChild(node);
         const element = document.getElementById("messageContainer2");
         element.appendChild(para);
-
-    }else if(chatBox3.style.display == 'block'){
+        chatContent2.value = ' ';
+        para.scrollIntoView();
+    }
+}
+function sendMessage3(){
+    if(chatContent3.value == " "){
+        alert("Input text");
+    }else{
+        (chatBox3.style.display == 'block')
         const para = document.createElement("p");
         const node = document.createTextNode(chatContent3.value);
         para.appendChild(node);
         const element = document.getElementById("messageContainer3");
         element.appendChild(para);
+        chatContent3.value = ' ';
+        para.scrollIntoView();
     }
 }
