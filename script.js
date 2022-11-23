53b2d9c92771a4488cf0066fd4e044a7f094fb1b
hiuwehf <matheus.thomassen@outlook.com>
2022-11-23 16:45:22 +0100

    cleaning

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const chatName = document.getElementById("chatNameBar");
 const chatBox0 = document.getElementById("chatBox0");
-const chatContainer1 = document.getElementById("chatContainer1");
-const chatContainer2 = document.getElementById("chatContainer2");
-const chatContainer3 = document.getElementById("chatContainer3");
+// const chatContainer1 = document.getElementById("chatContainer1");
+// const chatContainer2 = document.getElementById("chatContainer2");
+// const chatContainer3 = document.getElementById("chatContainer3");
+const chatBox1 = document.getElementById("chatBox1");
+const chatBox2 = document.getElementById("chatBox2");
+const chatBox3 = document.getElementById("chatBox3");
 const chatContent1 = document.getElementById("chatContent1");
 const chatContent2 = document.getElementById("chatContent2");
 const chatContent3 = document.getElementById("chatContent3");
@@ -13,25 +16,25 @@ const MessageContainer = document.getElementsByClassName("MessageContainer");
 function deezToggle(){
     chatName.innerHTML = "Deez";
     chatBox0.style.display="none";
-    chatContainer1.style.display="block";
-    chatContainer2.style.display="none";
-    chatContainer3.style.display="none";
+    chatBox1.style.display="block";
+    chatBox2.style.display="none";
+    chatBox3.style.display="none";
 }
 
 function ligmaToggle(){
     chatName.innerHTML = "Ligma";
     chatBox0.style.display="none";
-    chatContainer1.style.display="none";
-    chatContainer2.style.display="block";
-    chatContainer3.style.display="none";
+    chatBox1.style.display="none";
+    chatBox2.style.display="block";
+    chatBox3.style.display="none";
 }
 
 function sugondeseToggle(){
     chatName.innerHTML = "Sugondese";
     chatBox0.style.display="none";
-    chatContainer1.style.display="none";
-    chatContainer2.style.display="none";
-    chatContainer3.style.display="block";
+    chatBox1.style.display="none";
+    chatBox2.style.display="none";
+    chatBox3.style.display="block";
 }
 
 document.addEventListener('keydown', function (event){
@@ -40,13 +43,15 @@ document.addEventListener('keydown', function (event){
     }
 })
 
+
 function sendMessage1(){
     if(chatContent1.value != " "){
         (chatBox1.style.display == 'block')
         const para = document.createElement("p");
+        para.className = "messageSend";
         const node = document.createTextNode(chatContent1.value);
         para.appendChild(node);
-        const element = document.getElementById("messageContainerSend1");
+        const element = chatBox1;
         element.appendChild(para);
         chatContent1.value = ' ';
         para.scrollIntoView();
@@ -79,9 +84,10 @@ function recieveMessage(){
         const para = document.createElement("p");
         const node = document.createTextNode("Hello User");
         para.appendChild(node);
-        const element = document.getElementById("messageContainerRecieve1");
+        const element = chatBox1;
         element.appendChild(para);
         chatContent1.value = ' ';
+        para.className = "messageRecieve";
         para.scrollIntoView();
     }
 }
